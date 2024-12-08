069e28e6b097f6b0dcac876fa659064ddf221686
ron <1358612584@qq.com>
2024-12-08 11:44:30 +0800

    fix: 修复v-permission指令在某些情况下报错的问题

--- src/directive/permission/index.js
+++ src/directive/permission/index.js
@@ -3,16 +3,7 @@ import { checkPermission } from '@/utils/permission'
 function handlePermission(el, binding) {
   const { value } = binding
   const hasPermission = checkPermission(value)
-
-  if (!hasPermission) {
-    const parentNode = el.parentNode
-    // 对a-space组件下的组件进行特殊处理
-    if (parentNode.className.includes('arco-space-item')) {
-      parentNode.parentNode && parentNode.parentNode.removeChild(parentNode)
-    } else {
-      parentNode.removeChild(el)
-    }
-  }
+  el.style.display = hasPermission ? 'block' : 'none'
 }
 
 export default {
